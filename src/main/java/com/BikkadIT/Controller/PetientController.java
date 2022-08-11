c2f13cbe28036f1dff7486a036a06e535dc0b488
Mukesh Patil <mp743032@gmail.com>
2022-08-12 00:38:26 +0530

    SCRH-3

--- src/main/java/com/BikkadIT/Controller/PetientController.java
+++ src/main/java/com/BikkadIT/Controller/PetientController.java
@@ -1,8 +1,17 @@
 package com.BikkadIT.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.BikkadIT.Model.PetientInfo;
 import com.BikkadIT.Service.ServiceImpl;
 
 @RestController
@@ -10,4 +19,21 @@ public class PetientController {
 
 	@Autowired
 	private ServiceImpl serviceImpl;
+	
+	@PostMapping(value="/save" , consumes="application/json")
+	public ResponseEntity<String> save(@RequestBody PetientInfo petientInfo){
+		int savedata = serviceImpl.savedata(petientInfo);
+		if(savedata!=0) {
+			return new ResponseEntity<String>("Data save Successfully" , HttpStatus.CREATED);
+		}
+		return new ResponseEntity<String>("Data Not Saved", HttpStatus.BAD_REQUEST);
+	}
+	@GetMapping(value="/getData", produces="application/json")
+	public ResponseEntity<List<PetientInfo>> getAllData (){
+		List<PetientInfo> list = serviceImpl.getall();
+		
+		return new ResponseEntity<List<PetientInfo>>(list, HttpStatus.OK);
+	}
+	
+	
 }
